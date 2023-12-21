c78409ff099d7af80e770234e861c7253ed306aa
kames <kame.san.8341@gmail.com>
2023-12-21 11:26:46 +0900

    12/21

---
+++ lib/question.dart
@@ -0,0 +1,19 @@
+class Question {
+  final String text;
+  final bool answer;
+  final String explanation;
+
+  Question({
+    required this.text,
+    required this.answer,
+    required this.explanation,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    return Question(
+      text: json['text'] ?? '',
+      answer: json['answer'] ?? false,
+      explanation: json['explanation'] ?? '',
+    );
+  }
+}
